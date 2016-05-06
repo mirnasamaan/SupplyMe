5499f9b749339ff71f812209e8f2104c28884e7c
ODH-PC\ODH <m.ashraf@enlightworld.com>
2016-05-06 23:35:06 +0200

    Initial

---
+++ SupplyMe/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupplyMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.bodyClass = "login-body";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return null;
+        }
+
+        public ActionResult Request()
+        {
+            ViewBag.bodyClass = "request-body";
+            return View();
+        }
+
+    }
+}
